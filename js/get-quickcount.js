426e5bfb7351c3d1317d91f5618366e8a90c5ba9
Zaini Nijar <90563272+zaininijar@users.noreply.github.com>
2024-02-14 20:33:50 +0700

    Update get-quickcount.js

--- js/get-quickcount.js
+++ js/get-quickcount.js
@@ -313,9 +313,153 @@ const displayQuickcount4 = (data) => {
 
 getQuickcount4();
 
+
+// getQuickcount5
+
+const quickcount5Container = document.getElementById("quickcount-5");
+
+const getQuickcount5 = () => {
+    fetch('https://www.cnnindonesia.com/api/v2/external/pemilu2024?path=pilpres/2024/qc/summary/30')
+    .then(response => response.json())
+    .then(data => {
+        displayQuickcount5(data.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        quickcount5Container.innerText = 'Gagal memuat data';
+    });
+}
+
+const displayQuickcount5 = (data) => {
+
+    const html = `
+    <div class="container flex flex-col gap-4 px-4 py-8 border rounded">
+        <div class="mx-auto text-slate-900 px-8 py-2 font-bold text-xl bg-white rounded">
+            ${data.nama_lembaga}
+        </div>
+        <div>
+            <p class="text-lg">Data Masuk: ${data.intdatamasuk}%</p>
+            <p class="text-xs text-gray-500">Last Update: ${getHumanTime(data.dtlastupdate)}</p>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/1.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[0].inthasil}%</div>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/2.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[1].inthasil}%</div>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/3.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[2].inthasil}%</div>
+        </div>
+    </div>
+    `;
+    quickcount5Container.innerHTML = html;
+}
+
+getQuickcount5();
+
+
+// getQuickcount6
+
+const quickcount6Container = document.getElementById("quickcount-6");
+
+const getQuickcount6 = () => {
+    fetch('https://www.cnnindonesia.com/api/v2/external/pemilu2024?path=pilpres/2024/qc/summary/32')
+    .then(response => response.json())
+    .then(data => {
+        displayQuickcount6(data.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        quickcount6Container.innerText = 'Gagal memuat data';
+    });
+}
+
+const displayQuickcount6 = (data) => {
+
+    const html = `
+    <div class="container flex flex-col gap-4 px-4 py-8 border rounded">
+        <div class="mx-auto text-slate-900 px-8 py-2 font-bold text-xl bg-white rounded">
+            ${data.nama_lembaga}
+        </div>
+        <div>
+            <p class="text-lg">Data Masuk: ${data.intdatamasuk}%</p>
+            <p class="text-xs text-gray-500">Last Update: ${getHumanTime(data.dtlastupdate)}</p>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/1.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[0].inthasil}%</div>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/2.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[1].inthasil}%</div>
+        </div>
+        <div class="bg-indigo-500 rounded p-4 flex items-center gap-4">
+            <div
+            class="w-[80px] h-[80px] overflow-hidden position-relative rounded-full"
+            >
+            <img
+                class="object-cover object-center w-full h-full"
+                src="./img/3.png"
+                alt=""
+            />
+            </div>
+            <div class="text-4xl font-bold">${data.perolehan[2].inthasil}%</div>
+        </div>
+    </div>
+    `;
+    quickcount6Container.innerHTML = html;
+}
+
+getQuickcount6();
+
 setInterval(() => {
     getQuickcount1();
     getQuickcount2();
     getQuickcount3();
     getQuickcount4();
+    getQuickcount5();
+    getQuickcount6();
 }, 10000)
